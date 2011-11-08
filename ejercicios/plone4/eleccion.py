bf3c12e8254a7e1f298819007462bdc992e98783
Joaquín Rosales <globojorro@gmail.com>
2011-11-08 08:24:13 -0600

    Se agrega el código para definir el contenido llamado 'Eleccion'

---
+++ ejercicios/plone4/eleccion.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import unicodedata
+from logging import getLogger
+
+from five import grok
+from zope import schema
+from zope.component import getUtility
+
+from plone.directives import form
+from plone.registry.interfaces import IRegistry
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
+from plone.registry.interfaces import IRecordModifiedEvent
+
+log = getLogger('ejercicios.plone4')
+
+from ejercicios.plone4 import _
+
+
+class IEspeciales(form.Schema):
+    """Contenedor que permite relacionar contenido especial dentro del portal
+    """
+
+
+class IEleccion(form.Schema):
+    """Contenedor """
+    votos_total = schema.Float(
+                    title=_(u"Total de votos"),
+                    default=0.0,
+                    min=0.0,
+                    max=100.0,
+                    required=True,
+                    )
+
+
+class ICandidato(form.Schema):
+    votos = schema.Float(
+                        title=_(u"Votos obtenidos"),
+                        default=0.0,
+                        min=0.0,
+                        max=100.0,
+                        required=True,
+                        )
+    nombre = schema.TextLine(
+            title=_(u"Nombre(s) y apellido(s)"),
+            required=True,
+            default=u"",
+            )
+
+
+@grok.subscribe(IEleccion, IRecordModifiedEvent)
+def detectPriceChange(settings, event):
+    if record.fieldName == 'entryPrice':
+        log.warning("Someone change the price from %d to %d" % (event.oldValue, event.newValue,))
